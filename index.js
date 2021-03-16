7b4a98692e56c0060314da051867aec53de7b2ec
rfl626 <79843580+rfl626@users.noreply.github.com>
2021-03-16 21:11:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('DRAWL NAG\nWhatsApp Bot', {
+CFonts.say('TERMUX WA BOT\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`'Drawl Nag BOT' By @${package.author.name || package.author}`, {
+CFonts.say(`'TERMUX WA BOT' By @${package.author.name || package.author}`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
